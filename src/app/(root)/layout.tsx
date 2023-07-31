a7923e359f984308944a1f8e89294e9898a2a5fd
Ryan Yogan <ryanyogan@gmail.com>
2023-07-31 11:54:13 -0500

    basic sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/shared/navbar";
+import Sidebar from "@/components/shared/sidebar";
 
 export default function RootLayout({
   children,
@@ -8,6 +9,11 @@ export default function RootLayout({
   return (
     <div className="h-full">
       <Navbar />
+
+      <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
+        <Sidebar />
+      </div>
+
       <main className="md:pl-20 pt-16 h-full">{children}</main>
     </div>
   );
