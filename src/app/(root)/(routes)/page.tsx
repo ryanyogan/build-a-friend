f3b269c5bb18b63cc338466aecef85c381c33e9c
Ryan Yogan <ryanyogan@gmail.com>
2023-07-31 13:56:22 -0500

    category filter

--- src/app/(root)/(routes)/page.tsx
+++ src/app/(root)/(routes)/page.tsx
@@ -1,9 +1,14 @@
 import SearchInput from "@/components/inputs/search-input";
+import Categories from "@/components/list-views/categories";
+import { db } from "@/lib/prisma";
 
-export default function RootPage() {
+export default async function RootPage() {
+  const categories = await db.category.findMany();
+
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 }
