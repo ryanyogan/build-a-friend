53cd59f5297c344e2a196ef1a1eb469d7dd15268
Ryan Yogan <ryanyogan@gmail.com>
2023-07-31 12:21:53 -0500

    wip // mobile sidebar

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -2,10 +2,11 @@
 
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
-import { Menu, Sparkles } from "lucide-react";
+import { Sparkles } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import MobileSidebar from "./mobile-sidebar";
 import { ThemeToggle } from "./theme-toggle";
 
 const font = Poppins({
@@ -17,7 +18,8 @@ export default function Navbar() {
   return (
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu className="md:hidden block" />
+        {/* <Menu className="md:hidden block" /> */}
+        <MobileSidebar />
         <Link href="/">
           <h1
             className={cn(
