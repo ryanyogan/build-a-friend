3a63768adf954a6c349ddac6d6fd330a8bcfb6be
Ryan Yogan <ryanyogan@gmail.com>
2023-07-31 20:46:32 -0500

    skeleton loader for image upload

--- src/components/shared/image-upload.tsx
+++ src/components/shared/image-upload.tsx
@@ -19,7 +19,20 @@ export default function ImageUpload({
   useEffect(() => setMounted(true), []);
 
   if (!mounted) {
-    return null;
+    return (
+      <div className="space-y-4 w-full flex flex-col justify-center items-center">
+        <div className="p-4 border-4 border-dashed border-primary/10 rounded-lg hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center">
+          <div className="relative h-40 w-40">
+            <Image
+              alt="upload"
+              fill
+              src={"/placeholder.svg"}
+              className="rounded-lg object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
