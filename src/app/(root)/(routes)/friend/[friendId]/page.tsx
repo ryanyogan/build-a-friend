32f1a995998cdbbec87a4e388212b0a1105c22da
Ryan Yogan <ryanyogan@gmail.com>
2023-07-31 23:07:48 -0500

    fixed a security hole

--- src/app/(root)/(routes)/friend/[friendId]/page.tsx
+++ src/app/(root)/(routes)/friend/[friendId]/page.tsx
@@ -1,5 +1,6 @@
 import CreateFriendForm from "@/components/inputs/create-friend-form";
 import { db } from "@/lib/prisma";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
 
 interface FriendPageProps {
   params: {
@@ -11,10 +12,16 @@ export const revalidate = 60;
 
 export default async function FriendPage({ params }: FriendPageProps) {
   //TODO: Check pro subscription ;-)
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirectToSignIn();
+  }
+
   let friend;
   if (params.friendId) {
     friend = await db.friend.findUnique({
-      where: { id: params.friendId },
+      where: { id: params.friendId, userId },
     });
   }
 
