1e5d7f980364d094ced61fc9e9f8415095047f19
Ryan Yogan <ryanyogan@gmail.com>
2023-08-01 23:29:13 -0500

    mobile fixed

--- src/components/shared/mobile-sidebar.tsx
+++ src/components/shared/mobile-sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Menu } from "lucide-react";
-import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "../ui/sheet";
 import Sidebar from "./sidebar";
 
 export default function MobileSidebar() {
@@ -10,8 +10,13 @@ export default function MobileSidebar() {
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
-      <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
+      <SheetContent
+        side="left"
+        className="p-0 bg-secondary pt-10 w-32 flex justify-center"
+      >
+        <SheetClose>
+          <Sidebar />
+        </SheetClose>
       </SheetContent>
     </Sheet>
   );
